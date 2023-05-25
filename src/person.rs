7275b5b3e0241db001110feed7c56b190a16968e
Matt Thomas <mthomas@polybit.io>
2023-05-25 09:23:05 +0100

    Update JSON response to follow rocket v4 patterns

--- src/person.rs
+++ src/person.rs
@@ -1,9 +1,4 @@
-use std::io::Cursor;
-
 use rocket::serde::Deserialize;
-use rocket::response::{self, Responder, Response};
-use rocket::http::ContentType;
-use rocket::Request;
 use serde::Serialize;
 
 #[derive(Serialize, Deserialize)]
@@ -16,27 +11,4 @@ pub struct Person {
     pub last_name: String,
     // age is the age of the person.
     pub age: u8,
-}
-
-impl Person {
-    // Create a new person.
-    pub fn new() -> Person {
-        Person {
-            id: 0,
-            first_name: "".to_string(),
-            last_name: "".to_string(),
-            age: 0,
-        }
-    }
-}
-
-impl<'r> Responder<'r> for Person {
-    fn respond_to(self, _: &Request) -> response::Result<'r> {
-        Response::build()
-            .sized_body(Cursor::new(format!("{}:{}", self.name, self.age)), None)
-            .raw_header("X-Person-Name", self.name)
-            .raw_header("X-Person-Age", self.age.to_string())
-            .header(ContentType::new("application", "x-person"))
-            .ok()
-    }
 }
